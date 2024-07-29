fa26ab16fcd750cde2535ef20d45e440c54da314
Tejas Niwatkar <48887643+tejas3005@users.noreply.github.com>
2024-07-29 17:38:25 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bijanhotstar" # Replace with your actual S3 bucket name
+    bucket = "itvhotstar" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
